dfdf305269450e87cc762bddce00366f738aa286
Olivier Le Merdy <olivier.lemerdy+git@gmail.com>
2021-02-12 16:06:06 +0100

    Initial commit

---
+++ src/main/java/org/lafeuille/scalaspringboot/ScalaSpringBootPocApplication.java
@@ -0,0 +1,13 @@
+package org.lafeuille.scalaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalaSpringBootPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalaSpringBootPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lafeuille/scalaspringboot/ScalaSpringBootPocApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lafeuille.scalaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalaSpringBootPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
